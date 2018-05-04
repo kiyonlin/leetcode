9a1b2d9c67680b96ee5b09925859cfe1d56e0713
kiyon <kiyonlin@163.com>
2018-05-04 14:00:27 +0800

    101-c

--- algorithms/c/101.symmetric-tree.c
+++ algorithms/c/101.symmetric-tree.c
@@ -51,9 +51,8 @@ bool check(struct TreeNode* r1, struct TreeNode* r2)
 {
     if (r1 == NULL && r2 == NULL)
         return true;
-    if (r1 != NULL && r2 != NULL && r1->val == r2->val) {
+    if (r1 != NULL && r2 != NULL && r1->val == r2->val)
         return check(r1->left, r2->right) && check(r1->right, r2->left);
-    }
     return false;
 }
 
