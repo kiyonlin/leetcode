4f0b61bdc4c87f990c31854f43fc365b960edbc4
kiyon <kiyonlin@163.com>
2018-05-07 21:43:07 +0800

    237-c

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Thanks to [leetcode](https://leetcode.com) and [leetcode-cli](https://github.com
 
 | # | Title | Solution | Difficulty |
 |---| ----- | -------- | ---------- |
+|237|[Delete Node in a Linked List](https://leetcode.com/problems/delete-node-in-a-linked-list/description/)|[C](./algorithms/c/237.delete-node-in-a-linked-list.c)|Easy|
 |235|[Lowest Common Ancestor of a Binary Search Tree](https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/)|[C](./algorithms/c/235.lowest-common-ancestor-of-a-binary-search-tree.c)|Easy|
 |234|[Palindrome Linked List](https://leetcode.com/problems/palindrome-linked-list/description/)|[C](./algorithms/c/234.palindrome-linked-list.c)|Easy|
 |231|[Power of Two](https://leetcode.com/problems/power-of-two/description/)|[C](./algorithms/c/231.power-of-two.c)|Easy|
---
+++ algorithms/c/237.delete-node-in-a-linked-list.c
@@ -0,0 +1,36 @@
+/*
+ * [237] Delete Node in a Linked List
+ *
+ * https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+ *
+ * algorithms
+ * Easy (47.26%)
+ * Total Accepted:    206.8K
+ * Total Submissions: 437.6K
+ * Testcase Example:  '[0,1]\nnode at index 0 (node.val = 0)'
+ *
+ * 
+ * Write a function to delete a node (except the tail) in a singly linked list,
+ * given only access to that node.
+ * 
+ * 
+ * 
+ * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third
+ * node with value 3, the linked list should become 1 -> 2 -> 4 after calling
+ * your function.
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node)
+{
+    node->val = node->next->val;
+    struct ListNode* temp = node->next;
+    node->next = node->next->next;
+    free(temp);
+}
