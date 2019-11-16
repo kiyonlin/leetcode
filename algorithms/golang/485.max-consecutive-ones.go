1089a2915c11e3e43f4d3579c04ba46290c9528d
kiyon <kiyonlin@163.com>
2019-11-16 14:36:40 +0800

    golang 485

---
+++ algorithms/golang/485.max-consecutive-ones.go
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=485 lang=golang
+ *
+ * [485] Max Consecutive Ones
+ *
+ * https://leetcode.com/problems/max-consecutive-ones/description/
+ *
+ * algorithms
+ * Easy (55.08%)
+ * Total Accepted:    159.2K
+ * Total Submissions: 285.4K
+ * Testcase Example:  '[1,0,1,1,0,1]'
+ *
+ * Given a binary array, find the maximum number of consecutive 1s in this
+ * array.
+ * 
+ * Example 1:
+ * 
+ * Input: [1,1,0,1,1,1]
+ * Output: 3
+ * Explanation: The first two digits or the last three digits are consecutive
+ * 1s.
+ * ⁠   The maximum number of consecutive 1s is 3.
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The input array will only contain 0 and 1.
+ * The length of input array is a positive integer and will not exceed 10,000
+ * 
+ * 
+ */
+func findMaxConsecutiveOnes(nums []int) int {
+	max, count := 0, 0
+	for _, num := range nums {
+		if num == 1 {
+			count++
+			if count > max {
+				max = count
+			}
+		} else {
+			count = 0
+		}
+	}
+	return max
+}
