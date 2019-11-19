b41331cfce5e6215b6623c9290a0d5b97290e3c4
kiyon <kiyonlin@163.com>
2019-11-19 08:44:06 +0800

    golang 11

---
+++ algorithms/golang/11.container-with-most-water.go
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=11 lang=golang
+ *
+ * [11] Container With Most Water
+ *
+ * https://leetcode.com/problems/container-with-most-water/description/
+ *
+ * algorithms
+ * Medium (44.85%)
+ * Total Accepted:    487.8K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,8,6,2,5,4,8,3,7]'
+ *
+ * Given n non-negative integers a1, a2, ..., an , where each represents a
+ * point at coordinate (i, ai). n vertical lines are drawn such that the two
+ * endpoints of line i is at (i, ai) and (i, 0). Find two lines, which together
+ * with x-axis forms a container, such that the container contains the most
+ * water.
+ * 
+ * Note: You may not slant the container and n is at least 2.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In
+ * this case, the max area of water (blue section) the container can contain is
+ * 49. 
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ */
+func maxArea(height []int) int {
+	max := 0
+	i, j := 0, len(height)-1
+	for i < j {
+		area := min(height[i], height[j]) * (j - i)
+		if area > max {
+			max = area
+		}
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
+		}
+	}
+	return max
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
