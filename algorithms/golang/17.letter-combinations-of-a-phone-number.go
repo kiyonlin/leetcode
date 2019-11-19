cbc2e4a322eb2d1de3e56effd83de0d4cb594ea0
kiyon <kiyonlin@163.com>
2019-11-19 14:52:38 +0800

    golang 17

---
+++ algorithms/golang/17.letter-combinations-of-a-phone-number.go
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode id=17 lang=golang
+ *
+ * [17] Letter Combinations of a Phone Number
+ *
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (41.76%)
+ * Total Accepted:    477.9K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '"23"'
+ *
+ * Given a string containing digits from 2-9 inclusive, return all possible
+ * letter combinations that the number could represent.
+ * 
+ * A mapping of digit to letters (just like on the telephone buttons) is given
+ * below. Note that 1 does not map to any letters.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 
+ * 
+ * Note:
+ * 
+ * Although the above answer is in lexicographical order, your answer could be
+ * in any order you want.
+ * 
+ */
+var table = map[byte]string{
+	'2': "abc",
+	'3': "def",
+	'4': "ghi",
+	'5': "jkl",
+	'6': "mno",
+	'7': "pqrs",
+	'8': "tuv",
+	'9': "wxyz",
+}
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return nil
+	}
+	ret := []string{}
+	chars := table[digits[0]]
+	strs := letterCombinations(digits[1:])
+	for i := 0; i < len(chars); i++ {
+		if len(strs) == 0 {
+			ret = append(ret, string(chars[i]))
+		} else {
+			for _, s := range strs {
+				ret = append(ret, string(chars[i])+s)
+			}
+		}
+	}
+	return ret
+}
